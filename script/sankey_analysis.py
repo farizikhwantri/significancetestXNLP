b13c9d2f31a3e2d8df5d30819ea2bfb844b07612
farizikhwantri <farizikhwantri@gmail.com>
2026-08-30 10:41:34 +0200

    fix sankey visual

--- script/sankey_analysis.py
+++ script/sankey_analysis.py
@@ -4,17 +4,17 @@ import plotly.graph_objects as go
 nodes = [
     "Initial XAI Papers (818)",      # 0
     "Non-Eval / Other Venues",       # 1 (Discarded)
-    "Main Conf. Eval Papers (421)",  # 2
+    "Main Conf.+Finding Eval Papers (593)",  # 2
     "Misclassified/False Positives", # 3 (Discarded)
-    "Verified Corpus (326)",         # 4
-    "Significant Testing (97)",      # 5
-    "No Significant Testing (229)"   # 6
+    "Verified Corpus (494)",         # 4
+    "Significant Testing (125)",      # 5
+    "No Significant Testing (369)"   # 6
 ]
 
 # Define links (source, target, value)
 link_source = [0, 0, 2, 2, 4, 4]
 link_target = [1, 2, 3, 4, 5, 6]
-link_value  = [397, 421, 80, 326, 97, 244]
+link_value  = [593, 494, 99, 329, 125, 369]
 
 # Create figure
 fig = go.Figure(data=[go.Sankey(
